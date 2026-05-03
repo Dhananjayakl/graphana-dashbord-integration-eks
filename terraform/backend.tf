d3602f4242136fc487aff75a465c8a3fe4181ec3
@dhananjaya.kolpe <dhananjaya.kolpe@gmail.com>
2026-05-03 23:13:56 +0530

    changes to rectify the dev

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    # ── Replace these with your actual values ──────────────────────────────
-    bucket         = "dhan-terraform-state-bucket-2026" # existing S3 bucket
-    key            = "grafana-eks/terraform.tfstate"
-    region         = "us-east-1"               # your AWS region
-    dynamodb_table = "terraform-state-locking" # existing DynamoDB table
+    bucket         = "dhan-terraform-state-bucket-2026"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
 }
--- terraform/modules/grafana/outputs.tf
+++ terraform/modules/grafana/outputs.tf
@@ -1,7 +1,4 @@
-# modules/grafana/outputs.tf
 output "grafana_url" {
-  value = "http://${var.grafana_ec2_host}:${var.environment == "prod" ? 3001 : 3000}"
-}
-output "datasource_name" {
-  value = "Prometheus-${title(var.environment)}"
+  description = "Grafana dashboard URL"
+  value       = "http://${var.grafana_ec2_host}:${var.environment == "prod" ? 3001 : 3000}"
 }
